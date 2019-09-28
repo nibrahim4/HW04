e3e3fdddf237346fda840fdf17a8292b91bb9679
Nia Ibrahim <nibrahi3@uncc.edu>
2019-09-27 21:57:11 -0400

    Finished add movie code.

--- app/src/main/java/com/example/hw04/AddMovieActivity.java
+++ app/src/main/java/com/example/hw04/AddMovieActivity.java
@@ -2,7 +2,9 @@ package com.example.hw04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -10,6 +12,8 @@ import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class AddMovieActivity extends AppCompatActivity {
 
@@ -22,6 +26,8 @@ public class AddMovieActivity extends AppCompatActivity {
     public EditText et_imdb;
     public SeekBar sb_rating;
     public TextView tv_rating;
+    public Bundle extrasFromAddMovie;
+    //public ArrayList<Movie> moviesFromAdd = new ArrayList<Movie>();
 
 
     @Override
@@ -40,12 +46,12 @@ public class AddMovieActivity extends AppCompatActivity {
         tv_rating = findViewById(R.id.tv_rating);
 
 
-
-        String enteredName = et_Name.getText().toString();
-        String enteredDescription = ml_description.getText().toString();
-        String enteredYear = et_year.getText().toString();
-        String enteredIMDB = et_imdb.getText().toString();
         final String[] selectedGenre = {""};
+
+
+
+
+
 
         sb_rating.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -78,10 +84,32 @@ public class AddMovieActivity extends AppCompatActivity {
 
 
 
-        //Movie movie = new Movie(enteredName, enteredDescription)
+
         btn_CreateMovie.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                 //extrasFromMain = getIntent().getExtras().getBundle(MainActivity.MOVIE_KEY);
+
+
+                String enteredName = et_Name.getText().toString();
+                String enteredDescription = ml_description.getText().toString();
+                String enteredYear = et_year.getText().toString();
+                String enteredIMDB = et_imdb.getText().toString();
+                String selectedRating = tv_rating.getText().toString();
+
+                Movie movie = new Movie(enteredName, enteredDescription, selectedGenre[0], Integer.parseInt(selectedRating), Integer.parseInt(enteredYear), enteredIMDB);
+               // moviesFromAdd.add(movie);
+
+                //for(int i=0; i<extrasFromAddMovie.size(); i++){
+                  //  Log.d("Test", "onClick: " + extrasFromAddMovie.get(i).getName());
+                //}
+                Bundle sentData = new Bundle();
+                sentData.putSerializable("addedMovie",movie);
+                Intent intent = new Intent(AddMovieActivity.this, MainActivity.class);
+                intent.putExtra(MainActivity.MOVIE_KEY, sentData);
+
+                setResult(AddMovieActivity.RESULT_OK, intent);
                 finish();
             }
         });
--- app/src/main/java/com/example/hw04/MainActivity.java
+++ app/src/main/java/com/example/hw04/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.hw04;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,7 +16,10 @@ public class MainActivity extends AppCompatActivity {
 
     public Button btn_AddMovie;
     public ArrayList<Movie> movieList = new ArrayList<Movie>();
-    public final String MOVIE_KEY = "movies";
+    public static final String MOVIE_KEY = "movies";
+    public static final int REQ_CODE = 5;
+    public Bundle extrasFromAddMovie = new Bundle();
+    public Movie movie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +34,35 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intentToAddMovie = new Intent(MainActivity.this, AddMovieActivity.class);
-                Bundle bundle = new Bundle();
-                bundle.putSerializable(MOVIE_KEY, movieList);
-                intentToAddMovie.putExtra("toAddMovie", bundle);
-                startActivity(intentToAddMovie);
+                //Bundle bundle = new Bundle();
+               // bundle.putSerializable(MOVIE_KEY, movieList);
+               // intentToAddMovie.putExtra("toAddMovie", bundle);
+                startActivityForResult(intentToAddMovie, REQ_CODE);
             }
         });
 
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==REQ_CODE){
+            if(resultCode==RESULT_OK){
+
+                Log.d("TEST", "onActivityResult: " + data);
+                extrasFromAddMovie = data.getExtras().getBundle(MOVIE_KEY);
+
+                movie = (Movie) extrasFromAddMovie.getSerializable("addedMovie");
+
+                movieList.add(movie);
+
+
+                for (int i =0; i<movieList.size(); i++){
+                    Log.d("test", "onActivityResult: "+ movieList.get(i).getName());
+                }
+            }
+        }
     }
 }
