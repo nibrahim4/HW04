bc6941ecc39b679622d7376be268f7a452adb688
Nia Ibrahim <nibrahi3@uncc.edu>
2019-09-28 15:07:58 -0400

    added new icon images for movies by year activity.

--- app/src/main/java/com/example/hw04/MoviesbyRatingActivity.java
+++ app/src/main/java/com/example/hw04/MoviesbyRatingActivity.java
@@ -10,5 +10,6 @@ public class MoviesbyRatingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moviesby_rating);
+        setTitle("Movies by Rating");
     }
 }
--- app/src/main/java/com/example/hw04/MoviesbyYearActivity.java
+++ app/src/main/java/com/example/hw04/MoviesbyYearActivity.java
@@ -10,5 +10,6 @@ public class MoviesbyYearActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_moviesby_year);
+        setTitle("Movies by Year");
     }
 }
